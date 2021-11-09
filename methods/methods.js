1c65b9e7c5447432c114261c467c3fdf4460e785
Julian Cabrera <a.j.c.marce@gmail.com>
2021-11-09 13:15:38 -0300

    Separate screens and methods

--- methods/methods.js
+++ methods/methods.js
@@ -1,26 +1,4 @@
-
-export function calculate(stateSet){
-  if (global.screen == "161180+131282") {
-    global.screen = "";
-    stateSet(global.screen);
-    navigation.navigate('Vero + July');
-  }
-  else {
-    let p = new Promise((res, rej) => {
-      try {
-        global.screen = global.screen.replace(/x/g,'*');
-        global.screen = global.screen.replace(/÷/g,'/');
-        global.screen = realEval(global.screen);
-        res(global.screen);
-      }
-      catch(err){};
-    });
-    p.then((value)=>stateSet(value));
-
-  }
-}
-
-function realEval (str) {
+export function realEval (str) {
   let result = 0;
   let auxStr = eval(str).toString();
   
@@ -77,38 +55,28 @@ function isPrevPoint (str) {
 }
 
 
-export function C(stateSet){
-  global.screen ='';
-  stateSet(global.screen);
-}
-
-export function openParenthesis(stateSet) {
+export function openParenthesis() {
   if (!checkLast(global.screen,['0','1','2','3','4','5','6','7','8','9',')','.'])){
     global.screen += '(';
-    stateSet(global.screen);
   }
 }
 
-export function closeParenthesis(stateSet) {
+export function closeParenthesis() {
   if (!checkLast(global.screen,['x','÷','+','-','(','.']) & (global.screen != '') & parNotClosed(global.screen)){
     global.screen += ')';
-    stateSet(global.screen);
   } 
 } 
 
-export function operators(oper, stateSet) { 
+export function operators(oper) { 
   if (!checkLast(global.screen,['x','÷','+','-','(','.']) & (global.screen != '')){
     global.screen += oper;
-    stateSet(global.screen);
   }
   else if (checkLast(global.screen,['x','÷','+','-'])) {
     global.screen =  global.screen.slice(0, -1) + oper;
-    stateSet(global.screen);
-
   }
 }
 
-export function numbers(num, stateSet) {
+export function numbers(num) {
   let size = global.screen.length;
   if (size > 0) {
     
@@ -117,7 +85,6 @@ export function numbers(num, stateSet) {
     if (size == 1){
       if (last != '0') {
         global.screen += num;
-        stateSet(global.screen);
       }
     }
     if (size > 1) {
@@ -125,33 +92,30 @@ export function numbers(num, stateSet) {
 
       if (!check(last,[')']) & !check(lasttwo,['+0','-0','÷0','x0'])){
         global.screen+=num;
-        stateSet(global.screen);
       }
     }
   }
   else {
     if (num != '00') {
       global.screen += num;
-      stateSet(global.screen);
     }
   }
 }
 
-export function point(stateSet) {
+export function point() {
   let size = global.screen.length;
   if (size > 0) {
     let last = global.screen[size -1];
     if (size == 1) {
       if (!check(last,['+','-','÷','x','(',')','.'])) {
         global.screen += '.';
-        stateSet(global.screen);
       }
     }
     if (size > 1) {
       if (!isPrevPoint(global.screen) & !check(last,['+','-','÷','x','(',')'])) {
         global.screen += '.';
-        stateSet(global.screen);
       }
     }
   }
 }
+
--- screens/calculator.js
+++ screens/calculator.js
@@ -1,6 +1,13 @@
 import React, {useState} from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Text, View, TouchableOpacity, Image } from 'react-native';
+import {openParenthesis,
+        closeParenthesis,
+        numbers,
+        operators,
+        realEval} from '../methods/methods';
+
+
 
 export function Calculator({navigation}) {
   
@@ -23,145 +30,9 @@ export function Calculator({navigation}) {
     catch(err){};
     });
     p.then((value)=>setScreen(value));
-
-    }
-}
-
-function realEval (str) {
-  let result = 0;
-  let auxStr = eval(str).toString();
-  
-	if (!auxStr.search(/[0-9]*\.[0-9]*0000[1-9]/g)) {
-  	result = Number(auxStr.slice(0,-1));
-	}
-  else if (!auxStr.search(/[0-9]*\.[0-9]*9999[1-9]/g)) {
-    auxStr = auxStr.replace(/9*[0-9]$/g, "");
-    auxStr = auxStr.slice(0, - 1) + (Number(auxStr.slice(-1)) + 1).toString()
-    result = Number(auxStr);
-  }
-  else {
-    result = Number(auxStr);
-  }
-  
-  return result.toString();
-}
-
-function checkLast(str,arrayOfSymbols){
-  if (str.length > 0){
-    for (let i of arrayOfSymbols) {
-      if (str[str.length - 1] == i){
-        return true;
-      };
     }
   }
-  return false;
-}
 
-function check(str, comparissionArray){
-  for (let i of comparissionArray) {
-    if (str == i){
-      return true;
-    };
-  }
-  return false;
-}
-
-function parNotClosed(str){
-  return(((str.match(/\(/g) || []).length - (str.match(/\)/g) || []).length)>0)
-}
-
-function isPrevPoint (str) {
-  let strRev = str.split("").reverse().join("");
-  for (let i of strRev) {
-    if (check(i,['x','÷','+','-'])){
-      break;
-    }
-    else if (i == '.') {
-      return true;
-    }
-  }
-  return false;
-}
-
-
-function C(){
-  global.screen ='';
-  setScreen(global.screen);
-}
-
-function openParenthesis() {
-  if (!checkLast(global.screen,['0','1','2','3','4','5','6','7','8','9',')','.'])){
-    global.screen += '(';
-    setScreen(global.screen);
-  }
-}
-
-function closeParenthesis() {
-  if (!checkLast(global.screen,['x','÷','+','-','(','.']) & (global.screen != '') & parNotClosed(global.screen)){
-    global.screen += ')';
-    setScreen(global.screen);
-  } 
-} 
-
-function operators(oper) { 
-  if (!checkLast(global.screen,['x','÷','+','-','(','.']) & (global.screen != '')){
-    global.screen += oper;
-    setScreen(global.screen);
-  }
-  else if (checkLast(global.screen,['x','÷','+','-'])) {
-    global.screen =  global.screen.slice(0, -1) + oper;
-    setScreen(global.screen);
-
-  }
-}
-
-function numbers(num) {
-  let size = global.screen.length;
-  if (size > 0) {
-    
-    let last = global.screen[size -1];
-    
-    if (size == 1){
-      if (last != '0') {
-        global.screen += num;
-        setScreen(global.screen);
-      }
-    }
-    if (size > 1) {
-      let lasttwo = global.screen.slice(size - 2);
-
-      if (!check(last,[')']) & !check(lasttwo,['+0','-0','÷0','x0'])){
-        global.screen+=num;
-        setScreen(global.screen);
-      }
-    }
-  }
-  else {
-    if (num != '00') {
-      global.screen += num;
-      setScreen(global.screen);
-    }
-  }
-}
-
-function point() {
-  let size = global.screen.length;
-  if (size > 0) {
-    let last = global.screen[size -1];
-    if (size == 1) {
-      if (!check(last,['+','-','÷','x','(',')','.'])) {
-        global.screen += '.';
-        setScreen(global.screen);
-      }
-    }
-    if (size > 1) {
-      if (!isPrevPoint(global.screen) & !check(last,['+','-','÷','x','(',')'])) {
-        global.screen += '.';
-        setScreen(global.screen);
-      }
-    }
-  }
-}
 
   return (
     <View style={{flex: 1, justifyContent: 'flex-end', backgroundColor:'lightgrey'}}>
@@ -177,7 +48,7 @@ function point() {
             borderWidth: 1,
             backgroundColor: '#800040',
             justifyContent: 'center',
-            borderRadius: 20}} onPress={C}>
+            borderRadius: 20}} onPress={() => {global.screen = ''; setScreen(global.screen)}}>
             <Text style={{justifyContent: 'center',
               textAlign: 'center',
               fontFamily: 'sans-serif',
@@ -189,7 +60,7 @@ function point() {
             borderWidth: 1,
             backgroundColor: 'blue',
             justifyContent: 'center',
-            borderRadius: 20}} onPress={openParenthesis}><Text style={{
+            borderRadius: 20}} onPress={() => {openParenthesis(); setScreen(global.screen)}}><Text style={{
               justifyContent: 'center',
               textAlign: 'center',
               fontFamily: 'sans-serif',
@@ -201,7 +72,7 @@ function point() {
             borderWidth: 1,
             backgroundColor: 'blue',
             justifyContent: 'center',
-            borderRadius: 20}} onPress={closeParenthesis}><Text style={{
+            borderRadius: 20}} onPress={() => {closeParenthesis(); setScreen(global.screen)}}><Text style={{
               justifyContent: 'center',
               textAlign: 'center',
               fontFamily: 'sans-serif',
@@ -213,7 +84,7 @@ function point() {
             borderWidth: 1,
             backgroundColor: 'blue',
             justifyContent: 'center',
-            borderRadius: 20}} onPress={()=>{operators('÷')}}><Text style={{justifyContent: 'center',
+            borderRadius: 20}} onPress={() => {operators('÷'); setScreen(global.screen)}}><Text style={{justifyContent: 'center',
               textAlign: 'center',
               fontFamily: 'sans-serif',
               fontSize: 65, color: 'magenta'
@@ -221,18 +92,18 @@ function point() {
           </TouchableOpacity>
         </View>
         <View style={{flex: 0.25, flexDirection: 'row', backgroundColor: 'black'}}>
-          <TouchableOpacity style={styles.botones_def} onPress={()=>{numbers('7')}}><Text style={styles.botones_num_Texts}>7</Text>
+          <TouchableOpacity style={styles.botones_def} onPress={() => {numbers('7'), setScreen(global.screen)}}><Text style={styles.botones_num_Texts}>7</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.botones_def} onPress={()=>{numbers('8')}}><Text style={styles.botones_num_Texts}>8</Text>
+          <TouchableOpacity style={styles.botones_def} onPress={() => {numbers('8'), setScreen(global.screen)}}><Text style={styles.botones_num_Texts}>8</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.botones_def} onPress={()=>{numbers('9')}}><Text style={styles.botones_num_Texts}>9</Text>
+          <TouchableOpacity style={styles.botones_def} onPress={() => {numbers('9'), setScreen(global.screen)}}><Text style={styles.botones_num_Texts}>9</Text>
           </TouchableOpacity>
           <TouchableOpacity style={{
             flex: 0.25,
             borderWidth: 1,
             backgroundColor: 'blue',
             justifyContent: 'center',
-            borderRadius: 20}} onPress={()=>{operators('x')}}><Text style={{justifyContent: 'center',
+            borderRadius: 20}} onPress={() => {operators('x'); setScreen(global.screen)}}><Text style={{justifyContent: 'center',
               textAlign: 'center',
               fontFamily: 'sans-serif',
               fontSize: 40, color: 'magenta'
@@ -240,18 +111,18 @@ function point() {
           </TouchableOpacity>
         </View>
         <View style={{flex: 0.25, flexDirection: 'row', backgroundColor: 'black'}}>
-          <TouchableOpacity style={styles.botones_def} onPress={()=>{numbers('4')}}><Text style={styles.botones_num_Texts}>4</Text>
+          <TouchableOpacity style={styles.botones_def} onPress={() => {numbers('4'), setScreen(global.screen)}}><Text style={styles.botones_num_Texts}>4</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.botones_def} onPress={()=>{numbers('5')}}><Text style={styles.botones_num_Texts}>5</Text>
+          <TouchableOpacity style={styles.botones_def} onPress={() => {numbers('5'), setScreen(global.screen)}}><Text style={styles.botones_num_Texts}>5</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.botones_def} onPress={()=>{numbers('6')}}><Text style={styles.botones_num_Texts}>6</Text>
+          <TouchableOpacity style={styles.botones_def} onPress={() => {numbers('6'), setScreen(global.screen)}}><Text style={styles.botones_num_Texts}>6</Text>
           </TouchableOpacity>
           <TouchableOpacity style={{
             flex: 0.25,
             borderWidth: 1,
             backgroundColor: 'blue',
             justifyContent: 'center',
-            borderRadius: 20}} onPress={()=>{operators('-')}}><Text style={{justifyContent: 'center',
+            borderRadius: 20}} onPress={() => {operators('-'); setScreen(global.screen)}}><Text style={{justifyContent: 'center',
               textAlign: 'center',
               fontFamily: 'sans-serif',
               fontSize: 65, color: 'magenta'
@@ -259,18 +130,18 @@ function point() {
           </TouchableOpacity>
         </View>
         <View style={{flex: 0.25, flexDirection: 'row', backgroundColor: 'black'}}>
-          <TouchableOpacity style={styles.botones_def} onPress={()=>{numbers('1')}}><Text style={styles.botones_num_Texts}>1</Text>
+          <TouchableOpacity style={styles.botones_def} onPress={() => {numbers('1'), setScreen(global.screen)}}><Text style={styles.botones_num_Texts}>1</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.botones_def} onPress={()=>{numbers('2')}}><Text style={styles.botones_num_Texts}>2</Text>
+          <TouchableOpacity style={styles.botones_def} onPress={() => {numbers('2'), setScreen(global.screen)}}><Text style={styles.botones_num_Texts}>2</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.botones_def} onPress={()=>{numbers('3')}}><Text style={styles.botones_num_Texts}>3</Text>
+          <TouchableOpacity style={styles.botones_def} onPress={() => {numbers('3'), setScreen(global.screen)}}><Text style={styles.botones_num_Texts}>3</Text>
           </TouchableOpacity>
           <TouchableOpacity style={{
             flex: 0.25,
             borderWidth: 1,
             backgroundColor: 'blue',
             justifyContent: 'center',
-            borderRadius: 20}} onPress={()=>{operators('+')}}><Text style={{justifyContent: 'center',
+            borderRadius: 20}} onPress={() => {operators('+'); setScreen(global.screen)}}><Text style={{justifyContent: 'center',
               textAlign: 'center',
               fontFamily: 'sans-serif',
               fontSize: 55, color: 'magenta'
@@ -278,11 +149,11 @@ function point() {
           </TouchableOpacity>
         </View>
         <View style={{flex: 0.25, flexDirection: 'row', backgroundColor: 'black'}}>
-          <TouchableOpacity style={styles.botones_def} onPress={()=>{numbers('00')}}><Text style={styles.botones_num_Texts}>00</Text>
+          <TouchableOpacity style={styles.botones_def} onPress={() => {numbers('00'), setScreen(global.screen)}}><Text style={styles.botones_num_Texts}>00</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.botones_def} onPress={()=>{numbers('0')}}><Text style={styles.botones_num_Texts}>0</Text>
+          <TouchableOpacity style={styles.botones_def} onPress={() => {numbers('0'), setScreen(global.screen)}}><Text style={styles.botones_num_Texts}>0</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.botones_def} onPress={point}><Text style={styles.botones_num_Texts}>.</Text>
+          <TouchableOpacity style={styles.botones_def} onPress={() => {point(); setScreen(global.screen)}}><Text style={styles.botones_num_Texts}>.</Text>
           </TouchableOpacity>
           <TouchableOpacity style={{
             flex: 0.25,
